bb6edacdf645e627e08fb106e4d175393ceb2af3
Joel Jaun <jo.p.jaun@gmail.com>
2018-03-14 10:45:38 +0100

    Init
    Finish Chapter 6

---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
+
+@Component({
+  selector: 'bm-book-list',
+  templateUrl: './book-list.component.html',
+  styles: []
+})
+export class BookListComponent implements OnInit {
+  books: Book[];
+  @Output() showDetailsEvent = new EventEmitter<Book>();
+
+  constructor(private bs: BookStoreService) { }
+
+  ngOnInit() {
+    this.books = this.bs.getAll();
+  }
+
+  showDetails(book: Book) {
+    this.showDetailsEvent.emit(book);
+  }
+}
