fbefd4d9a9bde61c544684be6e8e07c5c73001cc
SoWebJaun <jo.p.jaun@gmail.com>
2018-03-19 06:31:08 +0100

    Weekend Commit

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
+import { BookFactory } from './../shared/book-factory';
 
 @Component({
   selector: 'bm-book-details',
@@ -10,19 +11,27 @@ import { BookStoreService } from '../shared/book-store.service';
   styles: []
 })
 export class BookDetailsComponent implements OnInit {
-  book: Book;
+  book: Book = BookFactory.empty();
 
   constructor(
     private bs: BookStoreService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
     const params = this.route.snapshot.params;
-    this.book = this.bs.getSingle(params['isbn']);
+    this.bs.getSingle(params['isbn']).subscribe(b => this.book = b);
   }
 
   getRating(num: number) {
     return new Array(num);
   }
+
+  removeBook() {
+    if (confirm('Buch wirklich löschen?')) {
+      this.bs.remove(this.book.isbn).subscribe(res => this.router.navigate(['../'], { relativeTo: this.route }));
+    }
+  }
+
 }
