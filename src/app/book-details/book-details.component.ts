d24784f3202b44dabb755b95e6c033596c8c4458
Joel Jaun <jo.p.jaun@gmail.com>
2018-04-11 12:11:28 +0200

    Revert "rxJs"
    This reverts commit a39c4426aa4108ffbb94396f9a2a4160b1cbf8ff.

---
+++ src/app/book-details/book-details.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
+import { BookFactory } from './../shared/book-factory';
+
+@Component({
+  selector: 'bm-book-details',
+  templateUrl: './book-details.component.html',
+  styles: []
+})
+export class BookDetailsComponent implements OnInit {
+  book: Book = BookFactory.empty();
+
+  constructor(
+    private bs: BookStoreService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    const params = this.route.snapshot.params;
+    this.bs.getSingle(params['isbn']).subscribe(b => this.book = b);
+  }
+
+  getRating(num: number) {
+    return new Array(num);
+  }
+
+  removeBook() {
+    if (confirm('Buch wirklich löschen?')) {
+      this.bs.remove(this.book.isbn).subscribe(res => this.router.navigate(['../'], { relativeTo: this.route }));
+    }
+  }
+
+}
